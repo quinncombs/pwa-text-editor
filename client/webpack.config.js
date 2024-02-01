7f23d12cde2e0803c068c54725d8db141ca331a4
quinncombs <qcombs31@gmail.com>
2024-02-01 18:22:29 -0500

    fixed webpack error and added dist folder

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,21 +20,21 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Text Generator'
+        title: 'JATE'
       }),
 
       new GenerateSW(),
       new WebpackPwaManifest({
-        name: 'TODOs',
-        short_name: 'TODOs',
-        description: 'Keep track of important tasks!',
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just another text editor',
         background_color: '#7eb4e2',
         theme_color: '#7eb4e2',
         start_url: './',
         publicPath: './',
         icons: [
           {
-            src: path.resolve('assets/images/logo.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
